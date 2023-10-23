e5cf9696a0955ba5186f32534d9063414b8fd137
Alexis <alexisfidalgofernandez@gmail.com>
2023-10-23 11:06:09 +0200

    update Ejercicio 5

--- src/main/java/com/campusdual/exercisespoo/Exercise05.java
+++ src/main/java/com/campusdual/exercisespoo/Exercise05.java
@@ -14,7 +14,7 @@ public class Exercise05 {
 
         esMúltiplo(numero, divisor);
 
-        int numero1= 11;
+        int numero1= 6;
         int numero2= 6;
 
         esMayor(numero1, numero2);
@@ -36,10 +36,12 @@ public class Exercise05 {
         }
     }
 public static void esMayor(int numero1, int numero2){
-        if (numero1 < numero2) {
+    if (numero1 < numero2) {
         System.out.println(numero1 + " es menor que " + numero2);
-        } else {
-        System.out.println(numero1 + " es mayor a " + numero2);
-        }
+    } else if (numero1 > numero2) {
+        System.out.println(numero1 + " es mayor que " + numero2);
+    } else {
+        System.out.println(numero1 + " es igual a " + numero2);
+    }
 }
 }
